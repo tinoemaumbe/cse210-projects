ce8fce25c581f0b3fcd23a2416b8cf1a40a572b4
tinoemaumbe <122685469+tinoemaumbe@users.noreply.github.com>
2023-05-26 16:22:20 +0200

    Update prompts.cs

--- prompts.cs
+++ prompts.cs
@@ -1,19 +1,13 @@
+public class Prompt
+{
+    public string _promptText;
 
-    class Entry
+    public Prompt(string promptText)
     {
-        //Entry properties
-        public DateTime Date { get; set; }
-        public int Prompt { get; set; }
-        public string Response { get; set; }
+        _promptText = promptText;
+    }
+}
 
-        //Entry constructor
-        public Entry(DateTime date, int prompt, string response)
-        {
-            Date = date;
-            Prompt = prompt;
-            Response = response;
-        }
-        //ToString method
         public override string ToString()
         {
             //Switch case for choosing prompt
@@ -22,40 +16,22 @@
             switch (Prompt)
             {
                 case 1:
-                    promptString = "Who was the most interesting person I interacted with today?";
+                    promptString = "Who was the most interesting person you met today?";
                     break;
                 case 2:
-                    promptString = "What was the best part of my day?";
+                    promptString = "What was the best part of your day?";
                     break;
                 case 3:
-                    promptString = "How did I see the hand of the Lord in my life today?";
+                    promptString = "How did you see the hand of the Lord in your life today?";
                     break;
                 case 4:
-                    promptString = "What was the strongest emotion I felt today?";
+                    promptString = "What was the strongest emotion you felt today?";
                     break;
                 case 5:
-                    promptString = "If I had one thing I could do over today, what would it be?";
+                    promptString = "If you had one thing you could do over today, what would it be?";
                     break;
             }
             return String.Format("{0:d} - {1}\n{2}", Date, promptString, Response);
         }
-        //ToFileString method
-
-        public string ToFileString()
-        {
-            return String.Format("{0:d}|{1}|{2}", Date, Prompt, Response);
-        }
-
-        //FromFileString method
-
-        public static Entry FromFileString(string fileString)
-        {
-            string[] parts = fileString.Split('|');
-            DateTime date = DateTime.Parse(parts[0]);
-            int prompt = int.Parse(parts[1]);
-            string response = parts[2];
-            return new Entry(date, prompt, response);
-        }
-    }
-
+    
 
