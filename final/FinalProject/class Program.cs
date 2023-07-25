9337732bd736998f2bca85c54a74577c20373d58
tinoemaumbe <122685469+tinoemaumbe@users.noreply.github.com>
2023-07-25 02:12:46 +0200

    Delete final/FinalProject directory

--- final/FinalProject/class Program.cs
+++
@@ -1,9 +0,0 @@
-public partial class Program
-{
-    static void Main(string[] args)
-    {
-        var atm = new ATM();
-        var console = new ATMConsole(atm);
-        console.Run();
-    }
-}
